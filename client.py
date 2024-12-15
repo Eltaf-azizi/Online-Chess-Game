579292456867266d184702510683c02f206974f6
Eltaf-azizi <iltfhuss@gmail.com>
2024-12-15 22:34:54 +0500

    correct the error of client.py

--- client.py
+++ client.py
@@ -1,37 +1,45 @@
 import socket
 import pickle
-
+import time
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "157.230.230.181"
+        self.host = "localhost"
         self.port = 5555
         self.addr = (self.host, self.port)
         self.board = self.connect()
         self.board = pickle.loads(self.board)
 
-
     def connect(self):
         self.client.connect(self.addr)
-        return self.client.recv(2048).decode()
-    
+        return self.client.recv(4096*8)
 
+    def disconnect(self):
+        self.client.close()
 
-    def send(self, data):
+    def send(self, data, pick=False):
         """
         :param data: str
         :return: str
         """
-        try:
-            self.client.send(pickle.dumps(data))
-            reply = self.client.recv(2048).decode()
-            return reply
-        
-        except socket.error as e:
-            return str(e)
-        
+        start_time = time.time()
+        while time.time() - start_time < 5:
+            try:
+                if pick:
+                    self.client.send(pickle.dumps(data))
+                else:
+                    self.client.send(str.encode(data))
+                reply = self.client.recv(4096*8)
+                try:
+                    reply = pickle.loads(reply)
+                    break
+                except Exception as e:
+                    print(e)
 
-n = Network()
-reply = n.send(n.board)
-print(pickle.loads(reply))
+            except socket.error as e:
+                print(e)
+
+
+        return reply
+
