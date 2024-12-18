e6370cbf0e72c979fad198e90feac54d54053653
Eltaf-azizi <iltfhuss@gmail.com>
2024-12-18 21:03:38 +0500

    added some changes to server.py

--- server.py
+++ server.py
@@ -1,69 +1,131 @@
 import socket
 from _thread import *
 from board import Board
+import pickle
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server = "157.230.230.181"
+server = "localhost"
 port = 5555
 
 server_ip = socket.gethostbyname(server)
 
-try: 
+try:
     s.bind((server, port))
 
 except socket.error as e:
     print(str(e))
 
+s.listen()
+print("[START] wWaiting for a connection")
 
-s.listen(2)
-print("waiting for a connection")
+connections = 0
+
+games = {0:Board(8, 8)}
+spector_ids = []
+specs = 0
+
+def read_specs():
+    global spectartor_ids
+    spectartor_ids = []
+    try:
+        with open("specs.txt", "r") as f:
+            for line in f:
+                spectartor_ids.append(line.strip())
+    except:
+        print("[ERROR] No specs.txt file found, creating one...")
+        open("specs.txt", "w")
+
+def threaded_client(conn, game, spec=False):
+    global pos, games, currentId, connections, specs
+    if not spec:
+        name = None
+        bo = games[game]
+        if connections % 2 == 0:
+            currentId = "w"
+        else:
+            currentId = "b"
+        bo.start_user = currentId
+        # Pickle the object and send it to the server
+        data_string = pickle.dumps(bo)
+        if currentId == "b":
+            bo.ready = True
+            bo.startTime = time.time()
+        conn.send(data_string)
+        connections += 1
 
 
-currentId = "w"
+        while True:
 
-def threaded_client(conn):
-
-    global currentId, pos
-    conn.send(str.encode(currentId))
-    currentId = "b"
-    reply = ''
-
-    while True:
-        try:
-            data = conn.recv(2048)
-            reply = data.decode('utf-8')
-
-            if not data:
-                conn.send(str.encode("Goodbye"))
+            if game not in games:
                 break
-            else:
-                print("Recieved: " + reply)
-                
 
-                if reply.count("w") == 1:
-                    nid = "b"
+            try:
+                d = conn.recv(8192*3)
+                data = d.decode("utf-8")
+
+                if not d:
+                    break
+
                 else:
-                    nid = "w"
+                    if data.count("select") > 0:
+                        all = data.split(" ")
+                        col = int(all[1])
+                        row = int(all[2])
+                        color = all[3]
+                        bo.select(col, row, color)
+                    
+                    if data == "winner b":
+                        bo.winner = "b"
+                        print("[GAME] Player b won in game", game)
+                    
+                    if data == "winner w":
+                        bo.winner = "w"
+                        print("[GAME] Player w won in game", game)
 
-                if id == 0: nid = 1
-                if id == 1: nid = 0
+                    if data == "update moves":
+                        bo.update_moves()
 
-                print("Sending: " + reply)
+                    if data.count("name") == 1:
+                        name = data.split(" ")[1]
+                        if currentId == "b":
+                            bo.p2Name = name
 
+                        elif currentId == "w":
+                            bo.p1Name = name
 
-            conn.sendall(reply)
+                        # print("Recieved board from", currentId, "in game", game)
+                        if bo.ready:
+                            if bo.turn == "w":
+                                bo.time1 = 900 - (time.time() - bo.startTime) - bo.storedTime1
+                            
+                            else:
+                                bo.time2 = 900 - (time.time() - bo.startTime) - bo.storedTime2
 
+                            sendData = pickle.dumps(bo)
+                            #print("Sending board to player", currentId, "in game", game)
+                        conn.sendall(sendData)
+
+            except Exception as e:
+                print(e)
+        connections -= 1
+
+        try:
+            del games[game]
+            print("[GAME] Game", game, "ended")
+        
         except:
-            break
+            pass
+            
+        print("[DISCONNECT] Player", name, "left game", game)
+        conn.close()
+    
+    else:
+        available_games = list(games.keys())
+        game_ind = 0
+        bo = games[available_games[game_ind]]
+        bo.start_user = "s"
+        data_string = pickle.dumps(bo)
 
-    print("Connection Closed")
-    conn.close()
 
-
-
-while True:
-    conn, addr = s.accept()
-    print("Connected to: ")
-
-    start_new_thread(threaded_client, (conn, ))
